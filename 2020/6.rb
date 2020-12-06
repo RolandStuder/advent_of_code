c60fbcd05daa4193b76df43b9979dd81c75a9424
Roland Studer <roland.studer@gmail.com>
2020-12-06 10:27:14 +0100

    Day 6 part 2

--- 2020/6.rb
+++ 2020/6.rb
@@ -9,8 +9,19 @@ class Group
   end
 
   def yes_count
-    @people.collect(&:answers).flatten.uniq.count
+    answered_yes.count
   end
+
+  def all_yes_count
+    answered_yes.count do |question|
+      @people.all? { |person| person.answers.include?(question) }
+    end
+  end
+
+  def answered_yes
+    @people.collect(&:answers).flatten.uniq
+  end
+
 end
 
 class Person
@@ -45,6 +56,12 @@ b
     sum_count = groups.collect(&:yes_count).sum
     assert_equal 11, sum_count
   end
+
+  def test_all_yes_example
+    groups = TEST_INPUT.split("\n\n").map { |entry| Group.new(entry)}
+    sum_count = groups.collect(&:all_yes_count).sum
+    assert_equal 6, sum_count
+  end
 end
 
 
@@ -56,3 +73,5 @@ puts sum_count
 
 puts "Solution 2"
 
+sum_all_yes_count = groups.collect(&:all_yes_count).sum
+puts sum_all_yes_count
