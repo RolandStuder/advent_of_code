7a552f94d944a2f5f61578f62ae8352f0e962bd5
Roland Studer <roland.studer@gmail.com>
2020-12-12 18:07:00 +0100

    Day 12 part 2

--- 2020/12.rb
+++ 2020/12.rb
@@ -49,6 +49,57 @@ class Ferry
   end
 end
 
+class WayPointFerry
+  attr_reader :x, :y, :waypoint_x_delta, :waypoint_y_delta
+
+  def initialize
+    @x = 0
+    @y = 0
+    @waypoint_x_delta = 10
+    @waypoint_y_delta = 1
+  end
+
+  def north(value)
+    @waypoint_y_delta += value
+  end
+
+  def south(value)
+    @waypoint_y_delta -= value
+  end
+
+  def west(value)
+    @waypoint_x_delta -= value
+  end
+
+  def east(value)
+    @waypoint_x_delta += value
+  end
+
+
+  # 90 degree turn to the right
+  #   -            -
+  # - 0 +x ==>  +y 0 -
+  #  +y           +x
+
+  def right(degrees)
+    (degrees / 90).times do
+      old_x_delta = @waypoint_x_delta
+      old_y_delta = @waypoint_y_delta
+      @waypoint_x_delta = old_y_delta
+      @waypoint_y_delta = -old_x_delta
+    end
+  end
+
+  def left(degrees)
+    right(360 - degrees)
+  end
+
+  def forward(value)
+    @x += value * @waypoint_x_delta
+    @y += value * @waypoint_y_delta
+  end
+end
+
 class CommandList
   attr_accessor :commands
 
@@ -67,9 +118,9 @@ class CommandList
     @commands = []
   end
 
-  def run
-    ferry = Ferry.new
-    commands.each do |command|
+  def run(from: 0, to: commands.size-1, with: Ferry)
+    ferry = with.new
+    commands[from..to].each do |command|
       ferry.send(command.command, command.value)
     end
     ferry
@@ -116,9 +167,36 @@ describe Ferry do
     assert_equal 17, ferry.x
     assert_equal 8, ferry.y # I actually think this is reversed, but don't care enough to change right now
   end
+
+  it "runs example list with WayPointFerry" do
+    list = CommandList.new_from_string(EXAMPLE_INPUT)
+    ferry = list.run(with: WayPointFerry)
+    assert_equal 214, ferry.x
+    assert_equal -72, ferry.y # I actually think this is reversed, but don't care enough to change right now
+  end
+
+  it "runs example list with WayPointFerry" do
+    list = CommandList.new_from_string(EXAMPLE_INPUT)
+    ferry = list.run(from: 0, to: 0, with: WayPointFerry)
+    assert_equal 100, ferry.x
+    assert_equal 10, ferry.y
+
+    ferry = list.run(from: 0, to: 1, with: WayPointFerry)
+    assert_equal 10, ferry.waypoint_x_delta
+    assert_equal 4, ferry.waypoint_y_delta
+
+    ferry = list.run(from: 0, to: 2, with: WayPointFerry)
+    assert_equal 170, ferry.x
+    assert_equal 38, ferry.y
+  end
 end
 
 puts "Solution 1"
 list = CommandList.new_from_string(File.read("12.dat"))
 ferry = list.run
+puts ferry.x.abs + ferry.y.abs
+
+puts "Solution 2"
+list = CommandList.new_from_string(File.read("12.dat"), )
+ferry = list.run(with: WayPointFerry)
 puts ferry.x.abs + ferry.y.abs
