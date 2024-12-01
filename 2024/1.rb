284b03904038ba9718e7094a9e48d3f6e309a7cf
Roland Studer <roli@vinebase.com>
2024-12-01 13:45:05 +0100

    2024, day 1, part 1, refactor

--- 2024/1.rb
+++ 2024/1.rb
@@ -9,22 +9,24 @@ example = <<~EXAMPLE
             3   3
 EXAMPLE
 
-def sum_of_distances(input)
+def sorted_lists(input)
   list_one = []
   list_two = []
   input.split("\n").each do |pair_string|
     list_one << pair_string.split.first.to_i
     list_two << pair_string.split.last.to_i
   end
+  return list_one.sort, list_two.sort
+end
 
-  list_one.sort!
-  list_two.sort!
-
+def sum_of_distances(list_one, list_two)
   distances = list_one.map.with_index do |elem, index|
     (elem - list_two[index]).abs
   end
   distances.sum
 end
 
-puts sum_of_distances(example)
-puts sum_of_distances(ParsingHelper.load(2024, 1).raw)
+example_list_one, example_list_two = sorted_lists(example)
+list_one, list_two = sorted_lists(ParsingHelper.load(2024, 1).raw)
+puts sum_of_distances(example_list_one, example_list_two)
+puts sum_of_distances(list_one, list_two)
